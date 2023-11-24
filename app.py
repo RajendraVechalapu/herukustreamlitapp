313bb9f75966082713d87f1c4c5f6d9e7e11bd86
Rajendra Vechalapu <vrsanaidu@gmail.com>
2023-11-25 11:11:30 +1300

    further changes

--- app.py
+++ app.py
@@ -1,29 +1,11 @@
-# simple_addition_app.py
-
 import streamlit as st
 
-def add_numbers(num1, num2):
-    """Function to add two numbers."""
-    result = num1 + num2
-    return result
+st.title('Greatest of Three Numbers') # Set Title of the webapp
 
-def main():
-    # Streamlit app title
-    st.title("Simple Addition App")
+choice1 = st.number_input('Enter First number') #Accepts a number input
+choice2 = st.number_input('Enter Second number')
+choice3 = st.number_input('Enter Third number')
 
-    # Input for the first number
-    num1 = st.number_input("Enter the first number:")
+string = f'Maximum value is {max(choice1,choice2,choice3)}'
 
-    # Input for the second number
-    num2 = st.number_input("Enter the second number:")
-
-    # Call the add_numbers function
-    result = add_numbers(num1, num2)
-
-    # Display the result
-    st.write(f"The result of {num1} + {num2} is: {result}")
-
-# Check if the script is being run directly
-if __name__ == "__main__":
-    # Call the main function to run the Streamlit app
-    main()
+st.write(string)
